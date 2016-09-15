b20d97040c89cbf54fbc48e4135c9692939c0b51
Andrei Frolov <frolov@sfu.ca>
2016-09-15 16:26:28 -0700

    Chebyshev and Legendre polynomials added.

--- 01 Orthogonal Polynomials/ortho.f90
+++ 01 Orthogonal Polynomials/ortho.f90
@@ -11,7 +11,7 @@ do i = 1,n
 	x(i) = (2*i-n-1.0)/(n-1.0)
 	y(i) = f(x(i))
 	
-	write (*,*) x(i), y(i), sin(2*pi*k*x(i)), ChebyshevT(k, x(i))
+	write (*,*) x(i), y(i), sin(2*pi*k*x(i)), ChebyshevT(k, x(i)), LegendreP(k, x(i))
 end do
 
 contains
@@ -28,5 +28,18 @@ pure function ChebyshevT(k, x)
 	ChebyshevT = cos(k*acos(x))
 end function
 
+pure function LegendreP(k, x)
+	real LegendreP, x; integer k; intent(in) k, x
+	real P(0:k); integer n
+	
+	P(0) = 1.0; if (k == 0) then; LegendreP = P(0); return; end if
+	P(1) = x;   if (k == 1) then; LegendreP = P(1); return; end if
+	
+	do n = 1,k-1
+		P(n+1) = ((2*n+1)*x*P(n) - n*P(n-1))/(n+1)
+	end do
+
+	LegendreP = P(k)
+end function
 
 end program
