f5743df2e7464fbb718bfa6b2e9069584fa5c5d2
Andrei Frolov <frolov@sfu.ca>
2016-09-20 14:24:45 -0700

    Gauss-Jordan elimination template.

---
+++ 01 Orthogonal Polynomials/gaussj.f90
@@ -0,0 +1,7 @@
+! solve A.x = B using Gauss-Jordan elimination
+! A gets destroyed, answer is returned in B
+subroutine gaussj(n, A, B)
+	integer n; real A(n,n), B(n)
+	
+	! ... actually solve it! ...
+end subroutine
