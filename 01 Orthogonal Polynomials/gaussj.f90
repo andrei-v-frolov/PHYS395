40cbe69d41f892af4f83ec57cf4c0eb5f0fac616
Andrei Frolov <frolov@sfu.ca>
2016-09-20 17:15:33 -0700

    Gauss-Jordan elimination template stripped of extra code.

--- 01 Orthogonal Polynomials/gaussj.f90
+++ 01 Orthogonal Polynomials/gaussj.f90
@@ -1,31 +1,25 @@
+! gaussj.f90  -  starting point for Gauss-Jordan elimination
+! compile with: gfortran -O3 -fdefault-real-8 gaussj.f90
+
+! the supplied code does neither work nor compiles, you should fix it yourself!
+
 program test; implicit none
 
 integer, parameter :: n = 2
+
 real A(n,n), B(n)
-
-integer i
-
-B = 0.0
-
-A = 0.0
 
 A(1,1) = 1.0
 A(1,2) = 7.0
 A(2,1) = 5.0
 A(2,2) = 1.0
-!A(3,1) = 7.0
-!A(3,3) = 1
 
 B(1) = 1.0
 B(2) = 3.0
 
-call lsolve(n, A, B)
+call gaussj(n, A, B)
+
 write (*,*) B
-
-
-!do i = 1,n
-!	write (*,*) A(i,:)
-!end do
 
 contains
 	
@@ -36,32 +30,19 @@ subroutine gaussj(n, A, B)
 	
 	integer i, j
 	
-	! ... actually solve it! ...
+	! get the matrix into upper-diagonal form first
 	do i = 1,n
+		! find a pivot element somewhere...
+		
 		do j = i+1,n
+			! eliminate a row along the lines of...
 			A(j,:) = A(j,:) - A(j,i)/A(i,i) * A(i,:)
-			!B(j,:) = B(j,:) - A(j,i)/A(i,i) * B(i,:)
-			write (*,*) i, j, A(j,:)
+			B(j,:) = B(j,:) - A(j,i)/A(i,i) * B(i,:)
 		end do
 	end do
-end subroutine
-
-! solve A.x = B using LAPACK canned routine
-! A gets destroyed, answer is returned in B
-subroutine lsolve(n, A, B)
-	integer n; real A(n,n), B(n)
 	
-	integer status, pivot(n)
-	
-        ! find static solution by direct inversion
-        status = 0; select case (kind(A))
-                case(4); call sgesv(n, 1, A, n, pivot, B, n, status)
-                case(8); call dgesv(n, 1, A, n, pivot, B, n, status)
-                case default; call abort
-        end select
-        
-        ! bail at first sign of trouble
-        if (status /= 0) call abort
+	! then solve the equations by back-substitution
+	! ...
 end subroutine
 
 end program
