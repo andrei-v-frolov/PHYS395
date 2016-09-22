83bb46567c63863c23547a74167db3e344d4d54e
Andrei Frolov <frolov@sfu.ca>
2016-09-22 16:40:45 -0700

    Comments cleaned up.

--- 02 Least Square Fits/read.f90
+++ 02 Least Square Fits/read.f90
@@ -6,15 +6,15 @@ real x(n), y(n)
 
 integer i, status
 
-! read beam from stdin
+! read data from stdin, at most n points
 do i = 1,n
 	read (*,*,iostat=status) x(i), y(i)
 	if (status < 0) exit
 end do
 
+! write data back to stdout
 do i = 1,n
 	write (*,*) x(i), y(i)
 end do
-
 
 end program
