69d923764a0da93b659af1a6f59c2b6886dd53a1
Andrei Frolov <frolov@sfu.ca>
2016-09-27 14:22:17 -0700

    Simple linear regression code added.

---
+++ 02 Least Square Fits/regression.f90
@@ -0,0 +1,30 @@
+program regression; implicit none
+
+real sxx, sxy, sy, sx, s
+real x, y, a, b
+integer status
+
+sxx = 0; sxy = 0; sx = 0; sy = 0; s = 0
+
+do
+	! read data from stdin
+	read (*,*,iostat=status) x, y
+	
+	! accumulate moments
+	sxx = sxx + x*x
+	sxy = sxy + x*y
+	sx = sx + x
+	sy = sy + y
+	s = s + 1.0
+	
+	! exit read loop if end of file reached
+	if (status < 0) exit
+end do
+
+! best fit values
+a = (s*sxy - sx*sy)/(s*sxx - sx*sx)
+b = -(sx*sxy - sxx*sy)/(s*sxx - sx*sx)
+
+write (*,*) a, b
+
+end program
