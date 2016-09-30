4dd97ecc8038ed999e0a9ae8e529a91527d85676
Andrei Frolov <frolov@sfu.ca>
2016-09-29 19:49:58 -0700

    SVD implemented.

--- 03 Principal Components/pca.py
+++ 03 Principal Components/pca.py
@@ -4,6 +4,9 @@
 import csv, datetime
 from dateutil import parser
 
+import numpy as np
+import scipy.linalg as linalg
+
 def stock(filename):
 	"""Parse CSV stock data, return a dictionary in {date: price} format"""
 	
@@ -25,10 +28,24 @@ apple = stock('APPL.csv')
 google = stock('GOOG.csv')
 microsoft = stock('MSFT.csv')
 
-# set of dates when we have proces for all three companies
+# set of dates when we have prices for all three companies
 common = set(apple.keys()) & set(google.keys()) & set(microsoft.keys())
 
-for date in common:
+data = []
+
+for date in sorted(common):
 	# date as seconds elapsed after epoch marker
 	secs = int(parser.parse(date).strftime('%s'))
-	print secs, apple[date], google[date], microsoft[date]
+	#print secs, apple[date], google[date], microsoft[date]
+	data.append([apple[date], google[date], microsoft[date]])
+
+# compute singular value decomposition of the stock data
+U, S, V = linalg.svd(data, compute_uv=True, overwrite_a=False)
+
+n = U.shape[0]; m = V.shape[0]; S[-1] = 0.0
+
+# recombine the matrix with some of the eigenvalues zeroed
+A = np.dot(U, np.dot(linalg.diagsvd(S,n,m), V))
+
+for row in A:
+	print row[0], row[1], row[2]
