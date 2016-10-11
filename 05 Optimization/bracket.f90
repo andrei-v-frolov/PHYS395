1456b460b7c4fa6a0a888eb8f183cb0cac19e271
Andrei Frolov <frolov@sfu.ca>
2016-10-11 14:10:15 -0700

    Minimum bracketing template.

---
+++ 05 Optimization/bracket.f90
@@ -0,0 +1,43 @@
+program bracketing; implicit none
+
+real x
+
+x = bracket(0.5, 1.5)
+
+write (*,*) x
+
+contains
+
+function bracket(a0, c0)
+	real a0, c0, bracket
+	real a, b, c, d, fa, fb, fc, fd
+	real, parameter :: eps = 1.0e-16
+	
+	a = a0; fa = f(a)
+	b = (a0 + c0)/2.0; fb = f(b) ! adjust!
+	c = c0; fc = f(c)
+	
+	!if ((fb-fa)*(fc-fb) > 0) call abort
+	if (fb >= fa .or. fb >= fc) call abort
+	
+	write (*,*) d, fd
+	
+	do while (abs(c-a) > eps)
+		d = (b+c)/2.0; fd = f(d) ! subdivide longest interval - bc *or* ab!
+		write (*,*) d, fd
+		
+		! test if abd or bdc bracket the minimum
+		if ((fb-fa)*(fd-fb) < 0.0) then; c = d; fc = fd; end if
+		if ((fd-fb)*(fc-fd) < 0.0) then; a = b; fa = fb; b = d; fb = fd; end if
+	end do
+	
+	bracket = b
+end function
+
+elemental function f(x)
+	real f, x; intent(in) x
+	
+	f = (x*x - 1.0)**2 + x
+end function
+
+end program
