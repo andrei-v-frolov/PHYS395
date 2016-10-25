1b9a6364bb7344123f11cacb8484b029731055e6
Andrei Frolov <frolov@sfu.ca>
2016-10-25 14:35:39 -0700

    Output Mathieu stability diagram as a FITS file.

--- 06 ODEs/floquet.f90
+++ 06 ODEs/floquet.f90
@@ -13,15 +13,26 @@ real, parameter :: P = 6.2831853071795864769252867665590057684Q0 ! period
 
 real, parameter :: dt = P/n
 
+integer, parameter :: na = 2**10, nq = 2**10
+real(8), parameter :: arange(2) = [-10.0,10.0], qrange(2) = [-10.0,10.0]
+
+real(4), allocatable :: image(:,:,:)
+
 integer i,j; real a, q; complex mu
 
-do i = -60,100; a = i/10.0
-        do j = -100,100; q = j/10.0
+allocate(image(2,na,nq))
+
+do i = 1,na; a = arange(1) + (arange(2) - arange(1))*(i-1)/(na-1)
+    do j = 1,nq; q = qrange(1) + (qrange(2) - qrange(1))*(j-1)/(nq-1)
                 mu = floquet()
-                write (*,'(4g24.16)') q, a, real(mu), aimag(mu)
+                !write (*,'(4g24.16)') q, a, real(mu), aimag(mu)
+                image(1,i,j) = real(mu)
+                image(2,i,j) = aimag(mu)
         end do
-        write (*,'(a)') ""
+        !write (*,'(a)') ""
 end do
+
+call write2fits('mathieu.fit', image, arange, qrange, ['ReMu', 'ImMu'], '(a,q)')
 
 contains
 
@@ -90,5 +101,61 @@ subroutine gl8(u, dt)
         ! update the solution
         u = u + matmul(g,b)*dt
 end subroutine gl8
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! write array data into FITS file as sequence of image extensions
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+subroutine write2fits(file, array, xx, yy, vars, coords)
+        character(len=*) file, vars(:), coords
+        real(4) array(:,:,:); real(8) xx(2), yy(2)
+        optional xx, yy, vars, coords
+        
+        integer i, j, status, unit
+        integer :: hdus, naxis = 2, n(2), npix
+        integer :: bitpix = -32, group = 1, blocksize = -1
+        
+        ! data dimansions
+        hdus = size(array,1)
+        n(1) = size(array,2)
+        n(2) = size(array,3)
+        npix = n(1)*n(2)
+        
+        ! delete file if it already exists
+        open(unit=1234, iostat=status, file=file, status='old')
+        if (status == 0) close(1234, status='delete'); status = 0
+        
+        ! initialize FITS file
+        call ftgiou(unit, status)
+        call ftinit(unit, file, blocksize, status)
+        
+        ! write image extensions
+        do i = 1,hdus
+                call ftiimg(unit, bitpix, naxis, n, status)
+                call ftppre(unit, group, 1, npix, array(i,:,:), status)
+                
+                if (present(vars)) then
+                        if (present(coords)) then
+                                call ftpkys(unit, 'EXTNAME', vars(i)//coords, 'variable stored in extension', status)
+                        else
+                                call ftpkys(unit, 'EXTNAME', vars(i), 'variable stored in extension', status)
+                        end if
+                end if
+                if (present(xx)) then
+                        call ftpkyj(unit, 'CRPIX1', 1, 'x-axis origin pixel', status)
+                        call ftpkyd(unit, 'CRVAL1', xx(1), 14, 'x-axis origin coordinate', status)
+                        call ftpkyd(unit, 'CDELT1', (xx(2)-xx(1))/n(1), 14, 'x-axis increment', status)
+                end if
+                if (present(yy)) then
+                        call ftpkyj(unit, 'CRPIX2', 1, 'y-axis origin pixel', status)
+                        call ftpkyd(unit, 'CRVAL2', yy(1), 14, 'y-axis origin coordinate', status)
+                        call ftpkyd(unit, 'CDELT2', (yy(2)-yy(1))/n(2), 14, 'y-axis increment', status)
+                end if
+        end do
+        
+        ! clean up
+        call ftclos(unit, status)
+        call ftfiou(unit, status)
+end subroutine write2fits
 
 end
