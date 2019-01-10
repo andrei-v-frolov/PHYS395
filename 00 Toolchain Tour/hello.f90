0535d71f6ea66db53d44d8a012143750b5719dd6
Andrei Frolov <frolov@sfu.ca>
2019-01-10 15:55:38 -0800

    Hello world example added.

---
+++ 00 Toolchain Tour/hello.f90
@@ -0,0 +1,16 @@
+program hello
+implicit none
+
+! number of points to print
+integer, parameter :: n = 100
+real x(0:n), y(0:n)
+integer i
+
+forall (i=0:n) x(i) = i/real(n)
+y = sin(10.0*x)
+
+do i = 0,n
+    write (*,*) x(i), y(i)
+end do
+
+end
