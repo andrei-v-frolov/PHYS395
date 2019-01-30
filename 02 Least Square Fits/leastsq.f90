abb4591c7a323a5e312ca61c760088408f169ad5
Andrei Frolov <frolov@sfu.ca>
2019-01-29 17:22:04 -0800

    SVD solver can handle degeneracies.

--- 02 Least Square Fits/leastsq.f90
+++ 02 Least Square Fits/leastsq.f90
@@ -33,7 +33,8 @@ write (*,*) A
 write (*,*) U
 
 ! solve for best fit parameters
-call lsolve(n, A, U)
+!call lsolve(n, A, U)
+call svdsolve(n, A, U, 1.0e-6)
 
 ! output best fit parameters
 write (*,*) U
@@ -44,7 +45,8 @@ contains
 subroutine evalb(x, B)
 	real x, B(n)
 	
-	B = [1.0, x, x*x]
+	! degenerate to illustrate SVD
+	B = [1.0, x, x+0.1]
 end subroutine
 
 ! solve A.x = B using LAPACK xGESV
@@ -64,6 +66,33 @@ subroutine lsolve(n, A, B)
 	
 	! abort at first sign of trouble
 	if (status /= 0) stop "singular matrix in lsolve()" 
+end subroutine
+
+! solve A.x = B using LAPACK xGESVD
+! A gets destroyed, answer is returned in B
+subroutine svdsolve(n, A, B, epsilon)
+	integer n, status; real A(n,n), B(n), epsilon
+	
+	! SVD matrices
+	real U(n,n), V(n,n), S(n), W(6*n)
+	
+	! initialize status to all clear
+	status = 0
+	
+	! call appropriate version of xGESV
+	select case (kind(A))
+		case(4); call sgesvd('A', 'A', n, n, A, n, S, U, n, V, n, W, 6*n, status)
+		case(8); call dgesvd('A', 'A', n, n, A, n, S, U, n, V, n, W, 6*n, status)
+		case default; call abort
+	end select
+	
+	! abort at first sign of trouble
+	if (status /= 0) stop "singular matrix in svdsolve()" 
+	
+	! compute the solution using pseudo-inverse
+	B = matmul(transpose(U),B)
+	where (S > epsilon*S(1)); B = B/S; elsewhere; B = 0.0; end where
+	B = matmul(transpose(V),B)
 end subroutine
 
 end program
