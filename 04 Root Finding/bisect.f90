f86e278f66af5c57feed65b5b9e82c0bc3f31f54
Andrei Frolov <frolov@sfu.ca>
2019-02-05 17:42:42 -0800

    Code commented for clarity.

--- 04 Root Finding/bisect.f90
+++ 04 Root Finding/bisect.f90
@@ -1,31 +1,34 @@
+! bisect.f90  -  find a (bracketed) root by bisection
+! compile with: gfortran -O3 -fdefault-real-8 bisect.f90
+
 program bisect
 implicit none
 
+! tolerance for interval length
 real, parameter :: epsilon = 1.0e-14
 
 real a, b, c, fa, fb, fc
 
+! initial interval
 a = 0.0; fa = f(a)
 b = 2.5; fb = f(b)
 
 if (fa*fb > 0.0) stop "The root is not bracketed, bailing out..."
 
+! bisect the interval
 do while (abs(b-a) > epsilon)
-	c = (a+b)/2.0; fc = f(c)
+	c = (a+b)/2.0; fc = f(c); if (fc == 0.0) exit
 	
 	write (*,*) c, fc
 	
-	if (fa*fc < 0.0) then
-		b = c; fb = fc
-	else
-		a = c; fa = fc
-	end if
+	if (fa*fc < 0.0) then; b = c; fb = fc; end if
+	if (fc*fb < 0.0) then; a = c; fa = fc; end if
 end do
 
 contains
 
-! function to find a root of
-function f(x)
+! function to find a root of...
+pure function f(x); intent(in) x
 	real f, x
 	f = cos(x)
 end function
--- 04 Root Finding/newton.f90
+++ 04 Root Finding/newton.f90
@@ -1,11 +1,16 @@
-program bisect
+! newton.f90  -  find a root by Newton's method
+! compile with: gfortran -O3 -fdefault-real-8 newton.f90
+
+program newton
 implicit none
 
 real x
 integer i
 
+! initial guess
 x = 1.0
 
+! Newton's iteration
 do i = 1,8
 	x = x - f(x)/df(x)
 	write (*,*) x, f(x)
@@ -13,14 +18,14 @@ end do
 
 contains
 
-! function to find a root of
-function f(x)
+! function to find a root of...
+pure function f(x); intent(in) x
 	real f, x
 	f = cos(x)
 end function
 
 ! derivative of a function to find a root of
-function df(x)
+pure function df(x); intent(in) x
 	real df, x
 	df = -sin(x)
 end function
