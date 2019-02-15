f25e6c7c714030539a5ec661206b212fb499001b
Andrei Frolov <frolov@sfu.ca>
2019-02-14 16:54:50 -0800

    Newton's method and regularized descent implemented.

--- 05 Optimization/minimize.f90
+++ 05 Optimization/minimize.f90
@@ -8,11 +8,11 @@ implicit none
 integer, parameter :: n = 2
 
 ! tilt of the potential bottom
-real, parameter :: mu = 1.0
+real, parameter :: mu = 0.1
 
 ! value of parameter determines effective damping
 ! this is very important to get right [more on this later]
-real, parameter :: dt = 0.001
+real, parameter :: dt = 1.0
 
 real x(n)
 integer i
@@ -21,8 +21,17 @@ integer i
 x = 0.0; x(1) = 2.0; x(2) = 1.0
 
 ! move in the downward direction
-do i = 1,10000
-	x = x - df(x)*dt
+do i = 1,20
+	! steepest descent: go down local gradient
+	! x = x - df(x)*dt
+	
+	! Newton's method: go to projected minimum location
+	! x = x - matmul(invert2x2(ddf(x)), df(x))*dt
+	
+	! Fletcher's regularization: modify Hessian matrix
+	! this is the basic building block in Levenberg–Marquardt algorithm
+	x = x - matmul(invert2x2(regular(ddf(x), 0.1)), df(x))*dt
+	
 	write (*,*) x, f(x)
 end do
 
@@ -48,5 +57,18 @@ pure function ddf(x); intent(in) x
 	forall (i=1:n) ddf(i,i) = ddf(i,i) + 4.0*(sum(x*x) - 1.0)
 end function
 
+! regularize matrix for inversion
+pure function regular(M, lambda); intent(in) M, lambda
+	real regular(n,n), M(n,n), lambda; integer i
+	
+	regular = M; forall (i=1:n) regular(i,i) = (1.0+lambda)*M(i,i)
+end function
+
+! invert 2x2 matrix
+pure function invert2x2(M); intent(in) M
+	real invert2x2(2,2), M(2,2)
+	
+	invert2x2 = reshape([M(2,2), -M(2,1), -M(1,2), M(1,1)]/(M(1,1)*M(2,2) - M(1,2)*M(2,1)), [2,2])
+end function
 
 end program
