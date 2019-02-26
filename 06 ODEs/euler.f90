2093a5b4438f47e60baf0e394a5c4ceb542160dd
Andrei Frolov <frolov@sfu.ca>
2019-02-26 15:07:18 -0800

    Euler method code factorized.

--- 06 ODEs/euler.f90
+++ 06 ODEs/euler.f90
@@ -1,23 +1,42 @@
+! euler.f90  -  Euler and Verlet methods demo
+! compile with: gfortran -O3 -fdefault-real-8 euler.f90
+
 program euler
 implicit none
 
 real, parameter :: omega = 1.0
 real, parameter :: dt = 0.1/omega
 
-real x, v, E
+real x, v, E0
 
 integer i
 
-x = 1.0; v = 0.0
+! initial conditions
+x = 1.0; v = 0.0; E0 = E(x,v)
 
+! time evolution
 do i = 1,1000
 	x = x + v*(dt/2.0)
-	v = v - (omega**2 * x) * dt
+	v = v + F(x,v)*dt
 	x = x + v*(dt/2.0)
 	
-	E = v**2/2.0 + omega**2 * x**2/2.0
-	
-	write (*,'(4g24.16)') i*dt, x, v, E
+	write (*,'(4g24.16)') i*dt, x, v, (E(x,v)-E0)/E0
 end do
+
+contains
+
+! force
+pure function F(x,v); intent(in) x, v
+	real x, v, F
+	
+	F = - omega**2 * x
+end function
+
+! energy
+pure function E(x,v); intent(in) x, v
+	real x, v, E
+	
+	E = v**2/2.0 + omega**2 * x**2/2.0
+end function
 
 end
