426aad03ee2aa6f7a3ffe0974de7d20b67818826
Andrei Frolov <frolov@sfu.ca>
2019-03-05 15:41:27 -0800

    Mandelbrot fractal - example of FITS I/O.

---
+++ 06 ODEs/mandelbrot.f90
@@ -0,0 +1,88 @@
+program mandelbrot
+implicit none
+
+! image size
+integer, parameter :: n = 1024
+
+! scan bounds
+real, parameter :: xx(2) = [-2.0, 2.0]
+real, parameter :: yy(2) = [-2.0, 2.0]
+
+! data array
+real(4) data(3,n,n)
+
+integer i, j
+
+data = 0.0
+
+! paraboloid
+forall (i=1:n, j=1:n) data(1,i,j) = (i-n/2.0)**2 + (j-n/2.0)**2
+
+! hyperboloid
+forall (i=1:n, j=1:n) data(2,i,j) = (i-n/2.0)**2 - (j-n/2.0)**2
+
+! checkerboard
+forall (i=1:n, j=1:n) data(3,i,j) = mod(i/64 + j/64,2)
+
+! write out image to file
+call write2fits('data.fit', data, xx, yy, ['red  ', 'green', 'blue '], '(x,y)')
+
+contains
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! write array data into FITS file as sequence of image extensions
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+subroutine write2fits(file, array, xx, yy, vars, coords)
+        character(len=*) file, vars(:), coords
+        real(4) array(:,:,:); real(8) xx(2), yy(2)
+        optional xx, yy, vars, coords
+        
+        integer i, j, status, unit
+        integer :: hdus, naxis = 2, n(2), npix
+        integer :: bitpix = -32, group = 1, blocksize = -1
+        
+        ! data dimansions
+        hdus = size(array,1)
+        n(1) = size(array,2)
+        n(2) = size(array,3)
+        npix = n(1)*n(2)
+        
+        ! delete file if it already exists
+        open(unit=1234, iostat=status, file=file, status='old')
+        if (status == 0) close(1234, status='delete'); status = 0
+        
+        ! initialize FITS file
+        call ftgiou(unit, status)
+        call ftinit(unit, file, blocksize, status)
+        
+        ! write image extensions
+        do i = 1,hdus
+                call ftiimg(unit, bitpix, naxis, n, status)
+                call ftppre(unit, group, 1, npix, array(i,:,:), status)
+                
+                if (present(vars)) then
+                        if (present(coords)) then
+                                call ftpkys(unit, 'EXTNAME', trim(vars(i))//coords, 'variable stored in extension', status)
+                        else
+                                call ftpkys(unit, 'EXTNAME', trim(vars(i)), 'variable stored in extension', status)
+                        end if
+                end if
+                if (present(xx)) then
+                        call ftpkyj(unit, 'CRPIX1', 1, 'x-axis origin pixel', status)
+                        call ftpkyd(unit, 'CRVAL1', xx(1), 14, 'x-axis origin coordinate', status)
+                        call ftpkyd(unit, 'CDELT1', (xx(2)-xx(1))/(n(1)-1), 14, 'x-axis increment', status)
+                end if
+                if (present(yy)) then
+                        call ftpkyj(unit, 'CRPIX2', 1, 'y-axis origin pixel', status)
+                        call ftpkyd(unit, 'CRVAL2', yy(1), 14, 'y-axis origin coordinate', status)
+                        call ftpkyd(unit, 'CDELT2', (yy(2)-yy(1))/(n(2)-1), 14, 'y-axis increment', status)
+                end if
+        end do
+        
+        ! clean up
+        call ftclos(unit, status)
+        call ftfiou(unit, status)
+end subroutine write2fits
+
+end program
---
+++ 06 ODEs/plot.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+# import libraries
+import matplotlib
+
+try:
+	import astropy.io.fits as pyfits
+except ImportError:
+	import pyfits
+import numpy as np
+
+#import scipy.stats
+from pylab import *
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+# load trajectory data
+hdu = pyfits.open('data.fit')
+
+data = hdu[0].data
+#green = hdu[1].data
+#blue = hdu[2].data
+
+ny,nx = data.shape
+
+# data extent and meshes
+x0 = hdu[0].header['CRVAL1']
+y0 = hdu[0].header['CRVAL2']
+dx = hdu[0].header['CDELT1']
+dy = hdu[0].header['CDELT2']
+
+x = x0 + arange(nx)*dx
+y = y0 + arange(ny)*dy
+
+X, Y = meshgrid(x, y)
+
+hdu.close()
+
+# create the figure
+figure(figsize=(10,8), frameon=False)
+
+# plot image data
+c = matplotlib.colors.LinearSegmentedColormap.from_list("difference", ["blue", "white", "red"])
+im = imshow(data, extent=(x[0],x[-1],y[0],y[-1]), origin='lower', aspect=dx/dy, cmap=c, interpolation='none')
+
+# plot contours
+contour(X, Y, data, 32, cmap=cm.jet)
+
+# make colorbar match the plot
+divider = make_axes_locatable(plt.gca())
+cax = divider.append_axes("right", size="5%", pad=0.07)
+plt.colorbar(im, cax=cax)
+
+show()
