8475e2cd85afeff91e6c42a027b6ee0a0a24daf2
Andrei Frolov <frolov@sfu.ca>
2019-03-05 20:12:17 -0800

    Optional anti-aliasing using median filter implemented.

--- 06 ODEs/plot.py
+++ 06 ODEs/plot.py
@@ -9,18 +9,24 @@ except ImportError:
 	import pyfits
 import numpy as np
 
-#import scipy.stats
 from pylab import *
+from scipy.signal import medfilt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 # load trajectory data
 hdu = pyfits.open('data.fit')
 
 data = hdu[0].data
-#green = hdu[1].data
-#blue = hdu[2].data
 
 ny,nx = data.shape
+
+# antialias using median filter
+osy = 1; osx = 1
+
+if (osy != 1 or osx != 1):
+	aakernel = [(osy & (~1)) + 1, (osx & (~1)) + 1]
+	data = medfilt(data, aakernel)[::osy,::osx]; ny,nx = data.shape
+	print("Antialiased with median kernel %s; output size is (%i,%i) pixels" % (aakernel,nx,ny))
 
 # data extent and meshes
 x0 = hdu[0].header['CRVAL1']
@@ -28,8 +34,8 @@ y0 = hdu[0].header['CRVAL2']
 dx = hdu[0].header['CDELT1']
 dy = hdu[0].header['CDELT2']
 
-x = x0 + arange(nx)*dx
-y = y0 + arange(ny)*dy
+x = x0 + arange(nx)*dx*osx
+y = y0 + arange(ny)*dy*osy
 
 X, Y = meshgrid(x, y)
 
@@ -40,7 +46,7 @@ figure(figsize=(10,8), frameon=False)
 
 # plot image data
 c = matplotlib.colors.LinearSegmentedColormap.from_list("difference", ["blue", "white", "black"])
-im = imshow(data, extent=(x[0],x[-1],y[0],y[-1]), origin='lower', aspect=dx/dy, cmap=c, interpolation='none')
+im = imshow(data, extent=(x[0],x[-1],y[0],y[-1]), origin='lower', aspect=(osx*dx)/(osy*dy), cmap=c, interpolation='none')
 
 # plot contours
 #contour(X, Y, data, 32, cmap=cm.jet)
