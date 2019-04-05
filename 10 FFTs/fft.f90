124e210ee61b5cb831cd7aaf759851fb98ad1990
Andrei Frolov <frolov@sfu.ca>
2019-04-05 14:48:02 -0700

    Code clean-up.

--- 10 FFTs/fft.f90
+++ 10 FFTs/fft.f90
@@ -8,8 +8,7 @@ include "fftw3.f"
 integer, parameter :: N = 2**10
 real, parameter :: pi = 3.141592653589793238462643383279502884197169399375Q0
 
-complex(8) in, ft, out
-dimension in(N), ft(N), out(N)
+complex(8) in(N), fft(N), out(N)
 integer*8 plan
 
 integer i, k
@@ -19,21 +18,21 @@ integer i, k
 !forall (i=1:N) in(i) = exp(2.0*sin(2*pi*(i-1)/N))
 forall (i=1:N) in(i) = exp( -1000.0*((i-N/2)*(1.0/N))**2 )
 
-call dfftw_plan_dft_1d(plan,N,in,ft,FFTW_FORWARD,FFTW_ESTIMATE)
-call dfftw_execute_dft(plan, in, ft)
+call dfftw_plan_dft_1d(plan,N,in,fft,FFTW_FORWARD,FFTW_ESTIMATE)
+call dfftw_execute_dft(plan, in, fft)
 call dfftw_destroy_plan(plan)
 
 do k = 1,N/2
-	ft(k) = (0,1)*(2.0*pi)*(k-1) * ft(k)		! positive frequencies
-	ft(n+1-k) = -(0,1)*(2.0*pi)*k * ft(n+1-k)	! negative frequensies
+	fft(k) = (0,1)*(2.0*pi)*(k-1) * fft(k)		! positive frequencies
+	fft(n+1-k) = -(0,1)*(2.0*pi)*k * fft(n+1-k)	! negative frequensies
 end do
 
-call dfftw_plan_dft_1d(plan,N,ft,out,FFTW_BACKWARD,FFTW_ESTIMATE)
-call dfftw_execute_dft(plan, ft, out)
+call dfftw_plan_dft_1d(plan,N,fft,out,FFTW_BACKWARD,FFTW_ESTIMATE)
+call dfftw_execute_dft(plan, fft, out)
 call dfftw_destroy_plan(plan)
 
 do i = 1,N
-	write (*,*) abs(in(i)), abs(ft(i)), real(out(i)/N)
+	write (*,*) abs(in(i)), abs(fft(i)), real(out(i)/N)
 end do
 
 end program
