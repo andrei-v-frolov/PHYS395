6ff24bd8c7b88bf9875495273044369e07f35d0d
Andrei Frolov <frolov@sfu.ca>
2024-01-18 13:45:49 -0800

    Compute orthogonal polynomials by recursion.

---
+++ 01 Orthogonal Polynomials/recursion.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# Compute orthogonal polynomials by recursion
+
+import numpy as np
+
+# order to expand to and number of points
+nmax = 10; pts = 101
+
+# evaluation grid
+x = np.linspace(-1.0, 1.0, pts)
+
+# initialize the array
+P = np.zeros([nmax,pts])
+
+# initialize the recursion relation
+P[0,:] = 1.0
+P[1,:] = x
+
+# Chebyshev: T[n+1] = 2x*T[n] - T[n-1]
+for n in range(1,nmax-1):
+	P[n+1,:] = 2.0*x*P[n,:] - P[n-1,:]
+
+# print the result
+print(P)
+
+
+#######################################################################
+
+import matplotlib.pyplot as plt
+
+for n in range(0,nmax):
+	plt.plot(x,P[n,:])
+
+plt.show()
