3503d3f826535ec346775257df6770ea021a7c1e
Andrei Frolov <frolov@sfu.ca>
2024-01-25 20:57:46 -0800

    Orthogonal polynomial expansion produces PDF figures.

--- 01 Orthogonal Polynomials/expansion.py
+++ 01 Orthogonal Polynomials/expansion.py
@@ -1,32 +1,33 @@
 #!/usr/bin/env python
 # compute orthogonal polynomial expansion coefficients
 
+#######################################################################
+
 import numpy as np
 from numpy.polynomial.legendre import legvander, legval
 from numpy.linalg import solve
+
+#######################################################################
 
 # number of coefficients
 n = 30
 
 # evaluation grid - uniform
-x = np.linspace(-1.0, 1.0, n)
+#x = np.linspace(-1.0, 1.0, n)
 
-# evaluation grid - Chebyshev
+# evaluation grid - cosine
 theta = np.linspace(np.pi, 0.0, n)
 x = np.cos(theta)
 
 # function to approximate
 f = np.exp(-x*x*4.5)
 
-# coefficients satisfy A*c = f
-A = legvander(x,n-1)
-c = solve(A,f)
+# coefficients satisfy f = B*c
+B = legvander(x,n-1); c = solve(B,f)
 
 #######################################################################
 
 import matplotlib.pyplot as plt
-
-#plt.plot(x,f,'g.')
 
 # test grid
 pts = 1000
@@ -38,4 +39,13 @@ y = legval(x,c)
 #plt.plot(x,f,'b-')
 plt.plot(x,y-f,'r-')
 
-plt.show()
+# restrict x axis range
+plt.xlim([-1.0,1.0])
+
+#plt.show()
+
+#######################################################################
+
+from os import environ
+environ['matplotlib.backend'] = 'pdf'
+plt.savefig("output.pdf", bbox_inches='tight', pad_inches=0.02, transparent=True)
