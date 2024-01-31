1fa6418089fbf9514926d1ce7f36f0cb645cbf6a
Andrei Frolov <frolov@sfu.ca>
2024-01-31 14:22:45 -0800

    Compute derivative using FFT.

--- 01 Orthogonal Polynomials/fft.py
+++ 01 Orthogonal Polynomials/fft.py
@@ -1,28 +1,44 @@
 #!/usr/bin/env python
-# compute fast Fourier transform of a function
+# compute (complex) fast Fourier transform of a function
 
 import numpy as np
 from scipy.fft import fft, ifft
 
 # number of points
-n = 32
+n = 1024
 
 # evaluation grid - uniform
 l = 1.0; dx = 2.0*l/n
-x = np.linspace(-1.0, 1.0-dx, n)
+x = np.linspace(-l, l-dx, n)
 
-# function to approximate
-f = np.exp(-x*x*4.5)
+# function to approximate (and its derivative)
+f = np.exp(-x*x*4.5); df = -9.0*x*f
 
 # compute (complex) Fourier transform
 F = fft(f); g = ifft(F)
+
+# compute frequency grid
+nn = n//2; dk = np.pi/l
+
+k = np.zeros(n)
+for i in range(0,nn+1):
+	k[i] = i*dk
+for i in range(nn+1,n):
+	k[i] = (i-n)*dk
+
+# compute derivative
+dg = ifft(1.0j*k*F)
 
 #######################################################################
 
 import matplotlib.pyplot as plt
 
-# plot residual
-plt.plot(x,f,'b.')
-plt.plot(x,np.real(g),'r-')
+# plot function residual
+#plt.plot(x,f,'b.')
+#plt.plot(x,np.real(g),'r-')
+
+# plot derivative residual
+#plt.plot(x,df,'b.')
+plt.plot(x,df-np.real(dg),'r-')
 
 plt.show()
