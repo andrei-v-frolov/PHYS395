56db769bb079506753e065a9d05d39fa56884ea6
Andrei Frolov <frolov@sfu.ca>
2024-01-31 14:37:21 -0800

    Frequency grid using build-in routine or iterators.

--- 01 Orthogonal Polynomials/fft.py
+++ 01 Orthogonal Polynomials/fft.py
@@ -2,7 +2,7 @@
 # compute (complex) fast Fourier transform of a function
 
 import numpy as np
-from scipy.fft import fft, ifft
+from scipy.fft import fft, ifft, fftfreq
 
 # number of points
 n = 1024
@@ -10,6 +10,7 @@ n = 1024
 # evaluation grid - uniform
 l = 1.0; dx = 2.0*l/n
 x = np.linspace(-l, l-dx, n)
+k = (2.0*np.pi) * fftfreq(n, dx)
 
 # function to approximate (and its derivative)
 f = np.exp(-x*x*4.5); df = -9.0*x*f
@@ -17,14 +18,9 @@ f = np.exp(-x*x*4.5); df = -9.0*x*f
 # compute (complex) Fourier transform
 F = fft(f); g = ifft(F)
 
-# compute frequency grid
-nn = n//2; dk = np.pi/l
-
-k = np.zeros(n)
-for i in range(0,nn+1):
-	k[i] = i*dk
-for i in range(nn+1,n):
-	k[i] = (i-n)*dk
+# explicitly compute frequency grid
+#nn = n//2; dk = np.pi/l
+#k = np.fromiter(((i-n if i>nn else i)*dk for i in range(0,n)), 'double')
 
 # compute derivative
 dg = ifft(1.0j*k*F)
