058785ad7091bf89924ad2328974f57ed3adac1c
Andrei Frolov <frolov@sfu.ca>
2024-02-02 15:17:10 -0800

    Specify distribution and parameters from command line.

--- 03 Statistics Primer/draw.py
+++ 03 Statistics Primer/draw.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python
-# draw a number of IID samples from a distribution
+# draw a specified number of IID samples from a distribution
 # run as: python draw.py PDF [dof:k] [samples:n] [seed:x]
 
 #######################################################################
 
+from sys import argv
+
 # default argument values
 pdf = "default"; initial = None; n = 32; dof = 7
+
+# select distribution (if specified)
+if len(argv) > 1:
+	pdf = argv[1]
+
+# parse parameters (if supplied)
+for p in argv[2:]:
+	match tuple(p.split(":")):
+		case ("dof",s):
+			dof = int(s)
+		case ("samples",s):
+			n = int(s)
+		case ("seed",s):
+			initial = int(s)
+		case _:
+			...
 
 #######################################################################
 
@@ -17,16 +35,23 @@ seed(initial)
 
 # draw samples from specified distribution
 match pdf:
+	# built-in generators
 	case "uniform":
 		x = uniform(size=n)
 	case "normal":
 		x = normal(size=n)
+	# aggregate distributions
 	case "sum":
 		x = np.sum(uniform(size=[n,dof]), 1)
 	case "max":
 		x = np.max(normal(size=[n,dof]), 1)
 	case "chi2":
 		x = np.sum(normal(size=[n,dof])**2, 1)
+	# shaping uniform PDF
+	case "box-muller":
+		u = rand(n); v = rand(n)
+		x = np.sqrt(-2.0*np.log(u)) * np.cos(2.0*np.pi*v)
+	# default random generator
 	case _:
 		x = rand(n)
 
