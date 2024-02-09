7575d16393b885ea396c41aacef1a225d70342b9
Andrei Frolov <frolov@sfu.ca>
2024-02-08 20:15:02 -0800

    Compute covariance of the supplied data, and draw Gaussian sample with the same...

---
+++ 03 Statistics Primer/covariance.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# compute the covariance of supplied data, and draw Gaussian sample with the same...
+# run as: python covariance.py < DATA > GAUSSIAN
+
+#######################################################################
+
+# operating system functions (for standard stream access)
+import sys
+
+# numerical libraries
+import numpy as np
+from numpy.random import normal
+from numpy.linalg import svd, cholesky
+
+#######################################################################
+
+# read in data from stdin
+data = np.loadtxt(sys.stdin)
+
+# sanity check on supplied data format
+n,columns = data.shape
+assert columns >= 2, ("Expecting at least 2 columns, got %i" % columns)
+
+# subtract average from the data (aka de-mean)
+for k in range(0,columns):
+	data[:,k] -= np.sum(data[:,k])/n
+
+# initialize covariance accumulator
+C = np.zeros([columns,columns])
+
+# accumulate covariance matrix
+for i in range(0,n):
+	x = data[i,:]
+	C += np.outer(x,x)
+
+# normalize to # of samples
+C /= n
+
+# compute correlation matrix
+D = np.diag(C); Q = C/np.sqrt(np.outer(D,D))
+
+#######################################################################
+
+# draw Gaussian samples with the same covariance
+L = cholesky(C); X = normal(size=[n,columns]) @ L.T
+
+# output them to stdout
+for i in range(0,n):
+	print(*X[i,:])
+
+#######################################################################
+
+'''
+# compute SVD of the covariance matrix
+U,S,VT = svd(C, hermitian=True)
+
+# junk all but the most prominent component
+S[1:] = 0.0
+
+# reconstruct the covariance matrix
+C = (U*S) @ VT
+'''
+
+#######################################################################
+
+import matplotlib.pyplot as plt
+
+plt.imshow(C, origin='lower', cmap='inferno')
+#plt.imshow(C, origin='lower', norm='log', cmap='inferno')
+
+plt.colorbar()
+plt.show()
