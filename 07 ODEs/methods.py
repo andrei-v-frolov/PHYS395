7c3f6a192771e6265676662ae5ab72ab60c19b6a
Andrei Frolov <frolov@sfu.ca>
2024-03-14 19:58:23 -0700

    Kepler problem added to tests.

--- 07 ODEs/methods.py
+++ 07 ODEs/methods.py
@@ -3,9 +3,12 @@
 
 #######################################################################
 
+from math import *
 import numpy as np
 from scipy.optimize import root
 
+#######################################################################
+# anharmonic oscillator (two-dimensional dynamic system example)
 #######################################################################
 
 # dimension of the state vector
@@ -26,6 +29,31 @@ def E(state):
 
 # initial conditions and energy
 state = np.array([1.0,0.0]); E0 = E(state)
+
+'''
+#######################################################################
+# Kepler problem (four-dimensional dynamic system example)
+#######################################################################
+
+# dimension of the state vector
+dim = 4
+
+# number and length of steps to take
+n = 2048; dt = 2.0*np.pi/16
+
+# dynamical system to be integrated
+def f(state):
+	x,y,vx,vy = state; r = sqrt(x*x+y*y)
+	return np.array([vx,vy,-x/r**3,-y/r**3])
+
+# energy (should be conserved for Hamiltonian EoM)
+def E(state):
+	x,y,vx,vy = state; r = sqrt(x*x+y*y)
+	return (vx*vx+vy*vy)/2.0 - 1.0/r
+
+# initial conditions and energy
+state = np.array([1.0,0.0,0.0,-1.0]); E0 = E(state)
+'''
 
 #######################################################################
 # Euler and Runge-Kutta methods
@@ -59,20 +87,34 @@ def rk4(state):
 # operator splitting methods for separable Hamiltonian (hard-coded EoM)
 #######################################################################
 
-# 1st order Hamiltonian split
+# 1st order Hamiltonian split (oscillator)
 def si1(state):
 	x,v = state
 	x += v*dt
 	v += -x**3*dt
 	return np.array([x,v])
 
-# 2nd order Hamiltonian split
+# 2nd order Hamiltonian split (oscillator)
 def si2(state):
 	x,v = state
 	x += v*dt/2.0
 	v += -x**3*dt
 	x += v*dt/2.0
 	return np.array([x,v])
+
+'''
+# 2nd order Hamiltonian split (Kepler problem)
+def si2(state):
+	x,y,vx,vy = state
+	x += vx*dt/2.0
+	y += vy*dt/2.0
+	r = sqrt(x*x+y*y)
+	vx -= x/r**3*dt
+	vy -= y/r**3*dt
+	y += vy*dt/2.0
+	x += vx*dt/2.0
+	return np.array([x,y,vx,vy])
+'''
 
 # to be continued...
 ...
@@ -99,4 +141,12 @@ import matplotlib.pyplot as plt
 
 #plt.plot(history[dim], history[0])
 plt.plot(history[dim], history[dim+1], "r-")
+
+'''
+#plt.plot(history[0], history[1])
+plt.gca().set_aspect('equal')
+plt.xlim([-1.5,1.5])
+plt.ylim([-1.5,1.5])
+'''
+
 plt.show()
