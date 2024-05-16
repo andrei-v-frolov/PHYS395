52fe74ffbf2a96a33d47146a4625fc0950039419
Andrei Frolov <frolov@sfu.ca>
2024-05-15 19:29:28 -0700

    Hamiltonian MC code added as option.

--- 03 Statistics Primer/mcmc-1.py
+++ 03 Statistics Primer/mcmc-1.py
@@ -16,6 +16,17 @@ x = uniform(-15.0, 15.0, size=chains)
 def likelihood(x):
 	return np.exp(-x*x/2.0)
 
+'''
+# random Hamiltonian kick, with dt ~ T/32
+def kick(x, pi=3.0, dt=0.02, steps=16):
+	x = x.copy(); vx = normal(scale=pi, size=chains)
+	for k in range(steps):
+		x += vx * (dt/2.0)
+		vx -= x * dt
+		x += vx * (dt/2.0)
+	return x
+'''
+
 # random step size (adjust for acceptance rate ~ 0.5)
 sigma = 0.3
 
@@ -23,6 +34,7 @@ sigma = 0.3
 history = np.zeros([n,chains])
 
 for i in range(0,n):
+	#y = kick(x)
 	y = x + normal(scale=sigma, size=chains)
 	alpha = likelihood(y)/likelihood(x)
 	
--- 03 Statistics Primer/mcmc-2.py
+++ 03 Statistics Primer/mcmc-2.py
@@ -3,57 +3,84 @@
 
 #######################################################################
 
-# number of steps and realizations
-n = 1024; chains = 1000; l = 1.5
-
 import numpy as np
 from numpy.random import normal, uniform
+
+#######################################################################
+
+# number of steps and realizations
+n = 1024; chains = 1000; l = 1.5
 
 # initial chain positions
 x = uniform(-l, l, size=[2,chains])
 
 # slightly tilted mexican hat likelihood
 def likelihood(x,y):
-	v = (x*x + y*y - 1.0)**2 + 0.05*y
-	return np.exp(-20.0*v)
+	U = 20.0*(x*x + y*y - 1.0)**2 + y
+	return np.exp(-U)
+
+'''
+# random Hamiltonian kick, with dt ~ T/32
+def kick(state, pi=5.0, dt=0.015, steps=16):
+	x,y = state.copy()
+	vx,vy = normal(scale=pi, size=[2,chains])
+	for k in range(steps):
+		x += vx * (dt/2.0); y += vy * (dt/2.0)
+		K = 80.0*(x*x + y*y - 1.0)
+		vx -= (K*x) * dt; vy -= (K*y+1.0) * dt
+		x += vx * (dt/2.0); y += vy * (dt/2.0)
+	return np.array([x,y])
+'''
 
 # random step size (adjust for acceptance rate ~ 0.5)
 sigma = 0.3
 
-# store chain history (you might want to downsample)
-history = np.zeros([n,2,chains])
-
-# MCMC using Metropolis-Hastings algorithm
-for i in range(0,n):
+# MCMC step using Metropolis-Hastings algorithm
+def step(x):
+	#y = kick(x)
 	y = x + normal(scale=sigma, size=[2,chains])
 	alpha = likelihood(y[0], y[1])/likelihood(x[0], x[1])
 	
 	u = uniform(size=chains) > alpha
-	x = np.where(np.reshape(u, [1,chains]), x, y)
-	history[i] = x
+	#print('acceptance rate = %.3f' % (1.0 - np.sum(u)/chains))
+	return np.where(np.reshape(u, [1,chains]), x, y)
+
+#for i in range(0,n): x = step(x)
 
 #######################################################################
 
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation as animate
 
 # sample likelihood on a grid
 grid = np.linspace(-l, l, 1024)
 X,Y = np.meshgrid(grid, grid)
 L = likelihood(X,Y)
 
-fig = plt.figure()
+fig = plt.figure(); ax = fig.gca()
 
 # density plot of likelihood
-plt.contourf(X,Y,L, levels=30, cmap='Blues')
+plt.imshow(L, cmap='Blues', origin='lower', extent=[-l,l,-l,l], interpolation='none')
+#plt.contourf(X,Y,L, levels=30, cmap='Blues')
 plt.colorbar()
 
-# end state of the chains
+# current state of the chains
 walkers = plt.scatter(x[0],x[1], marker=".", color="tab:red", s=5.0)
+#walkers = plt.scatter(x[0],x[1], c=np.arctan2(x[0],x[1]), cmap='plasma', marker=".", s=5.0)
 
-#animation = animate(fig, lambda i: walkers.set_offsets(history[i].T), n, interval=1000/60)
+# plot extent
+ax.set_aspect('equal')
+plt.xlim([-l,l])
+plt.ylim([-l,l])
 
-plt.xlim([-l,l]); plt.ylim([-l,l])
-plt.gca().set_aspect('equal')
+#######################################################################
+
+import matplotlib.animation as animation
+
+# called to advance animation to next frame
+def animate(i):
+	global x; x = step(x)
+	walkers.set_offsets(x.T)
+
+animation = animation.FuncAnimation(fig, animate, frames=1000, interval=1000/60)
 #animation.save('mcmc.mp4')
 plt.show()
