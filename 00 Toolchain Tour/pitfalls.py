d997ac1e7ce97eb24f51b43bd91377f899448835
Andrei Frolov <frolov@sfu.ca>
2025-01-14 21:20:00 -0800

    Range symmetrized.

--- 00 Toolchain Tour/pitfalls.py
+++ 00 Toolchain Tour/pitfalls.py
@@ -15,11 +15,12 @@ i = 1 << 72; n = np.array([1]) << 72; print(i,n[0])
 
 # limited float precision can lead to unexpected results
 n = 1000000
-a = np.array([0], dtype='int64')
-b = np.array([0], dtype='float32')
-for i in range(n):
-	a += n-i; b += n-i
+a = np.array([0,0], dtype='int64')
+b = np.array([0,0], dtype='float32')
+for i in range(n+1):
+	a += [i,n-i]; b += [i,n-i]
 print(a[0],b[0])
+print(a[1],b[1])
 
 # exception handling by NumPy is different!
 print(np.zeros(1)/0.0)
