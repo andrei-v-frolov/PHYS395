c04ec8bf26219ca59126e526f72830239054ace6
Andrei Frolov <frolov@sfu.ca>
2025-01-21 15:55:47 -0800

    Compute orthogonal polynomials by recursion.

--- 01 Orthogonal Polynomials/README.md
+++ 01 Orthogonal Polynomials/README.md
@@ -1,3 +1,7 @@
 # Representing Functions
 
 - `spline.py` - compute cubic spline approximation
+
+### Orthogonal Polynomials
+
+- `recursion.py` - compute orthogonal polynomials by recursion
---
+++ 01 Orthogonal Polynomials/recursion.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# compute orthogonal polynomials by recursion
+
+#######################################################################
+
+import numpy as np
+from time import perf_counter as now
+
+# order to expand to and number of points
+n = 5; pts = 1024
+
+# evaluation grid
+x = np.linspace(-1.0, 1.0, pts)
+
+# allocate the array
+#P = np.empty([n+1,pts])
+
+# it is safer to initialize it!
+P = np.zeros([n+1,pts])
+
+# time the computation
+t1 = now()
+
+# initialize the recursion relation
+P[0] = 1.0
+P[1] = x
+
+# recursion for Chebyshev and Legendre polynomials
+for k in range(1,n):
+	#P[k+1] = 2.0*x*P[k] - P[k-1]
+	P[k+1] = (2*k+1)/(k+1)*x*P[k] - k/(k+1)*P[k-1]
+
+# wall time elapsed (note formatted string literal!)
+t2 = now()
+print(f'Recursion takes {t2-t1}s to complete')
+
+#######################################################################
+
+from numpy.polynomial.legendre import legvander
+
+# time the computation
+t1 = now()
+
+# direct evaluation of Chebyshev polynomials
+#theta = np.arccos(x)
+#B = np.array([np.cos(k*theta) for k in range(n+1)])
+
+# direct evaluation of Legendre polynomials
+B = legvander(x,n).T
+
+# wall time elapsed
+t2 = now()
+print(f'Evaluation takes {t2-t1}s to complete')
+
+#######################################################################
+
+import matplotlib.pyplot as plt
+
+plt.figure()
+
+plt.plot(x, B.T, '-', linewidth=3)
+#plt.plot(x, P[n]-B[n], 'r-')
+
+plt.xlim([-1.0,1.0])
+
+# show in interactive console
+plt.show()
