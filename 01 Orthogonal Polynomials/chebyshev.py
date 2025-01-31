31ca5fe176429273c49cf124e06f82ab863dd38a
Andrei Frolov <frolov@sfu.ca>
2025-01-30 17:17:39 -0800

    Fast Chebyshev transform demo.

---
+++ 01 Orthogonal Polynomials/chebyshev.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# compute fast Chebyshev transform of a function
+
+#######################################################################
+
+import numpy as np
+from scipy.fft import dct, idct, idst
+
+# number of grid points
+n = 256
+
+# cosine evaluation grid - EXCLUDING endpoints
+dt = np.pi/n; theta = np.linspace(np.pi-dt/2, dt/2, n)
+l = 1.0; x = l * np.cos(theta); k = np.arange(n)/l
+
+#######################################################################
+
+# GENERIC test function (and its derivative)
+f = np.exp(-x*x*4.5); df = -9.0*x*f
+
+# PERIODIC test function (and its derivative)
+#y = np.sin(np.pi*x/(2*l)); z = np.cos(np.pi*x/(2*l)) 
+#f = np.exp(-y*y*4.5); df = -(9.0*np.pi/(2*l))*y*z*f
+
+#######################################################################
+
+# compute Type-II discrete cosine transform
+F = dct(f); g = idct(F)
+
+# compute Chebyshev derivative using Type-II DST
+dg = -idst(np.roll(k*F,-1))/np.sin(theta)
+
+#######################################################################
+
+import matplotlib.pyplot as plt
+
+#plt.plot(x, f)
+plt.plot(x, dg-df, "r-")
+
+# restrict x axis range
+plt.xlim([-l,l])
+
+# show in interactive console
+plt.show()
