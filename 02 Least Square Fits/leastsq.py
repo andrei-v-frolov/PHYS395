f6f042b266c2badd875a36465d6c15dbf109a155
Andrei Frolov <frolov@sfu.ca>
2025-02-04 17:50:54 -0800

    Optional point weights implemented.

--- 02 Least Square Fits/leastsq.py
+++ 02 Least Square Fits/leastsq.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # generalized least square fit to supplied data
+# expected data format: x f [sigma]
 
 #######################################################################
 
@@ -9,27 +10,34 @@ from numpy.polynomial.chebyshev import chebvander, chebval
 
 #######################################################################
 
-# number of coefficients
-n = 30
+# number of coefficients to fit
+n = 30; epsilon = 0.0e-3
 
 # load data from file (TBD: read from stdin)
 data = np.loadtxt('data.txt')
 
-# data to be fitted (TBD: add point weights)
+# sanity check on supplied data format
+pts,columns = data.shape
+assert columns >= 2, f'Expecting at least 2 columns, got only {columns}'
+
+# data to be fitted
 x = data[:,0]
 f = data[:,1]
+
+# optional weights
+W = 1.0/data[:,2]**2 if columns > 2 else np.ones(pts)
 
 # expansion basis
 B = chebvander(x,n-1)
 
 # NumPy has built-in solver minimizing |B*c-f|^2
-#c,*r = lstsq(B,f,rcond=1.0e-3)
+#c,*r = lstsq(B,f,rcond=epsilon)
 
 #######################################################################
 
 # it is better to cast the problem to nxn matrix as
-A = np.matmul(B.T, B)
-y = np.matmul(B.T, f)
+A = np.matmul(B.T*W, B)
+y = np.matmul(B.T*W, f)
 
 # explicit regularization could be added, like so
 #A += 1.0*np.diag(np.arange(n)**2)
@@ -38,13 +46,13 @@ y = np.matmul(B.T, f)
 c = solve(A,y)
 
 # A could well be degenerate, use SVD to handle that
-#U,S,V = svd(A, full_matrices=False, hermitian=True)
+#U,S,V = svd(A, hermitian=True)
 #print(f'SVD accuracy {norm(U*S@V - A)/norm(A)}')
 #print(f'Condition number is {S.max()/S.min():g}')
-#c = V.T*np.where(S > 1.0e-6*S[0], 1.0/S, 0.0) @ U.T @ y
+#c = V.T*np.where(S > epsilon**2*S[0], 1.0/S, 0.0) @ U.T @ y
 
 # this is equivalent to calling
-#c,*r = lstsq(A,y,rcond=1.0e-6)
+#c,*r = lstsq(A,y,rcond=epsilon**2)
 
 # evaluate best fit
 g = chebval(x, c)
@@ -54,7 +62,7 @@ g = chebval(x, c)
 import matplotlib.pyplot as plt
 
 plt.plot(x, f)
-plt.plot(x, g)
+plt.plot(x, g, linewidth=3)
 
 # restrict x axis range
 plt.xlim([-1.0,1.0])
