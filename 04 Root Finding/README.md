5477aa1db2edcf6210b5289d125e41879ae2160b
Andrei Frolov <frolov@sfu.ca>
2025-02-26 21:26:07 -0800

    First look at Newton's fractal.

--- 04 Root Finding/README.md
+++ 04 Root Finding/README.md
@@ -2,4 +2,5 @@
 
 - `bracket.py` - find a bracketed root by bisection and friends
 - `newton.py` - find a root by Newton's method
+- `fractal.py` - basin of convergence for Newton's method
 - `builtin.py` - demo of root finders in `scipy.optimize`
---
+++ 04 Root Finding/fractal.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# convergence of Newton's method (fractal for z^3=1)
+
+#######################################################################
+
+# numerical libraries
+import numpy as np
+
+#######################################################################
+
+# resolution and interval to sample
+n = 1024; l = 3.0
+
+# grid of starting values
+re = np.linspace(-l, l, n)
+im = np.linspace(-l, l, n)
+a,b = np.meshgrid(re,im)
+
+# form a complex-valued array
+z = a + b*1j
+
+#######################################################################
+
+# simplest way to see the structure
+for i in range(8):
+	z -= (z**3-1)/(3.0*z**2)
+
+# not the prettiest, but servicable
+q = np.abs(z**3-1) + 1.0e-6
+
+#######################################################################
+
+import matplotlib.pyplot as plt
+
+plt.imshow(q, origin='lower', extent=[-l,l,-l,l], norm='log', cmap='twilight', interpolation='none')
+
+# set square aspect if desired
+#plt.gca().set_aspect('equal')
+#plt.xlim([-l,l]); plt.ylim([-l,l])
+
+plt.colorbar()
+plt.show()
+
