364248b1afda6462d23abdc03873256d00847982
Andrei Frolov <frolov@sfu.ca>
2025-02-27 19:11:08 -0800

    Instrument function calls to plot optimization history.

--- 05 Optimization/minimize.py
+++ 05 Optimization/minimize.py
@@ -10,9 +10,16 @@ from scipy.optimize import minimize
 # tilt parameter
 mu = 0.1
 
+# call history
+history = []
+
 # tilted Mexican hat (testing convergence in winding valley)
-def f(u):
-	x,y = u; return (x*x+y*y - 1.0)**2/4.0 - mu*x
+def f(v):
+	global history; history.append(v)
+	x,y = v; return (x*x+y*y - 1.0)**2/4.0 - mu*x
+
+def df(v):
+	x,y = v; return [(x*x+y*y - 1.0)*x - mu, (x*x+y*y - 1.0)*y]
 
 #######################################################################
 
@@ -21,9 +28,41 @@ start = [0.0,2.0]
 
 # if not supplied, gradient is evaluated by finite difference
 result = minimize(f, start); print("SciPy default", result)
-result = minimize(f, start, method='BFGS'); print("BFGS", result)
-result = minimize(f, start, method='L-BFGS-B'); print("L-BFGS-B", result)
-result = minimize(f, start, method='CG'); print("Conjugate gradient", result)
+result = minimize(f, start, method='BFGS', jac=df); print("BFGS", result)
+result = minimize(f, start, method='L-BFGS-B', jac=df); print("L-BFGS-B", result)
+result = minimize(f, start, method='CG', jac=df); print("Conjugate gradient", result)
 result = minimize(f, start, method='Powell'); print("Powell's method", result)
 result = minimize(f, start, method='Nelder-Mead'); print("Nelder-Mead method", result)
 
+#######################################################################
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# domain to be plotted
+l = 1.5; pts = 1024; dl = l/pts
+
+# function being minimized
+grid = np.linspace(-l+dl,l-dl,pts)
+X,Y = np.meshgrid(grid,grid)
+F = np.vectorize(lambda x,y: f([x,y]))(X,Y)
+
+# scale function for plotting
+F = F/(mu*mu+F*F)**0.375
+
+# re-run specific optimizer to plot
+history = []
+minimize(f, start, method='BFGS', jac=df)
+history = np.array(history)
+
+# plot optimization history
+plt.plot(history[:,0], history[:,1], "r.-")
+#plt.imshow(F, extent=[-l,l,-l,l], cmap='Blues')
+plt.contourf(F, extent=[-l,l,-l,l], levels=13, cmap='Blues')
+plt.colorbar()
+
+# make sure aspect is 1:1
+plt.gca().set_aspect('equal')
+plt.xlim([-l,l]); plt.ylim([-l,l])
+
+plt.show()
