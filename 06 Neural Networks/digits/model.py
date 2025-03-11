35c6e6b46a33eb23343ec9068497b89a6f3067b3
Andrei Frolov <frolov@sfu.ca>
2025-03-10 21:51:24 -0700

    Simplify model declaration.

--- 06 Neural Networks/digits/model.py
+++ 06 Neural Networks/digits/model.py
@@ -19,8 +19,8 @@ device = (
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
-        self.flatten = nn.Flatten()
-        self.linear_sigmoid_stack = nn.Sequential(
+        self.stack = nn.Sequential(
+            nn.Flatten(),
             nn.Linear(28*28, 11*11),
             nn.Sigmoid(),
             nn.Linear(11*11, 5*5),
@@ -29,6 +29,5 @@ class NeuralNetwork(nn.Module):
         )
 
     def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_sigmoid_stack(x)
+        logits = self.stack(x)
         return logits
